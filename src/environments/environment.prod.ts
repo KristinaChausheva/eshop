adaf283469b645b94b1584938e0b1ee5b1472eb4
Kristina Chausheva <k.chausheva@gmail.com>
2020-02-25 03:04:13 +0200

    fisrt commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDN0pb1uGAfAV4tBdJIy3m8WqjZALTTM6o",
+    authDomain: "eshop-c9654.firebaseapp.com",
+    databaseURL: "https://eshop-c9654.firebaseio.com",
+    projectId: "eshop-c9654",
+    storageBucket: "eshop-c9654.appspot.com",
+    messagingSenderId: "312235514856",
+    appId: "1:312235514856:web:d0ef0c49e626f2a283e25d",
+    measurementId: "G-EBQXKJHBYV"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDN0pb1uGAfAV4tBdJIy3m8WqjZALTTM6o",
+    authDomain: "eshop-c9654.firebaseapp.com",
+    databaseURL: "https://eshop-c9654.firebaseio.com",
+    projectId: "eshop-c9654",
+    storageBucket: "eshop-c9654.appspot.com",
+    messagingSenderId: "312235514856",
+    appId: "1:312235514856:web:d0ef0c49e626f2a283e25d",
+    measurementId: "G-EBQXKJHBYV"
+  }
 };
 
 /*
